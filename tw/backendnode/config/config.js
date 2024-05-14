811f8a2f092c9cc39fdf509e4721e3737354412f
JJacobMS <montielsalasjesus@gmail.com>
2024-05-13 23:21:19 -0600

    tw

---
+++ tw/backendnode/config/config.js
@@ -0,0 +1,25 @@
+module.exports =
+{
+  "development": {
+    "username": 'netflix_user2',
+    "password": 'contrasena',
+    "database": 'netflixnode',
+    "host": 'localhost',
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
